0a4361908eefdf6ccd09662f12f99d7d3f467229
Tom <xdmyssy@gmail.com>
2023-05-04 19:35:56 +0200

    fix

--- ahcal_event_display.sh
+++ ahcal_event_display.sh
@@ -3,12 +3,12 @@
 source /mnt2/SJTU/Song/conda_setup/conda.env
 conda activate cepc_tb
 
-file_path=/mnt2/AHCAL/PublicAna/2023/BeamAna/result/mu-/100GeV/AHCAL_Run19_20230425_131356.root
+file_path=/home/songsy/TBEventDisplayv2/data/AHCAL/PublicAna/2023/BeamAna/result/pi+/AHCAL_Run45_20221021_010542.root
 save_dir=./Result
-entry_start=1000
-entry_end=2000
+entry_start=0
+entry_end=None
 random_num=10
 
 pid=True
 
-python ahcal.py --file_path $file_path --save_dir $save_dir --entry_start $entry_start --entry_end $entry_end  --pid $pid --random_num $random_num
+python ahcal.py --file_path $file_path --save_dir $save_dir --entry_start $entry_start  --pid $pid --random_num $random_num
